7aacab48da439dec7ecf3a544702529ef726d98d
MauAcer <ayala.mauricio.2004@gmail.com>
2025-11-06 14:20:46 -0300

    requirements x2

--- database.py
+++ database.py
@@ -2,14 +2,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
 
 # Datos de conexión (ajustá a tu entorno)
-DB_USER = "postgres"
-DB_PASSWORD = "7606"
-DB_HOST = "localhost"
-DB_NAME = "Sifen_API"
+load_dotenv()
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+ENV = os.getenv("ENV")
+
+if ENV == "Produccion" :
+    DATABASE_URL = os.getenv("DATABASE_URL")
+else :
+    DB_USER = "postgres"
+    DB_PASSWORD = "7606"
+    DB_HOST = "localhost"
+    DB_NAME = "Sifen_API"
+    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
